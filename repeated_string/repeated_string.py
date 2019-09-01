44e2f5fbfdd4855736df0d773356e02ea336a09b
Joseph I. Abraham <jia253@nyu.edu>
2019-09-01 14:13:18 -0400

    Create repeated_string.py

---
+++ repeated_string/repeated_string.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    
+    single_string_a_count = 0
+    complete_string_repeats = int(n/len(s))
+
+    for i in range (0, len(s)):
+        if (s[i:i+1] == 'a'):
+            single_string_a_count += 1
+    
+    complete_string_a_count = (single_string_a_count)*complete_string_repeats
+    total_a_count = complete_string_a_count
+    incomplete_repeat_length = n - (complete_string_repeats*len(s))
+
+    for i in range(len(s)-incomplete_repeat_length, len(s)):
+        if (s[i:i+1] == 'a'):
+            total_a_count += 1
+    
+    return total_a_count
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
