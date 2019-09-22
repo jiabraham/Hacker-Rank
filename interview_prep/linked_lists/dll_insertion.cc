3eb6b0b59f66bb6054a718db4e6724cad6b532fc
Joseph I. Abraham <jia253@nyu.edu>
2019-09-22 12:17:06 -0400

    Create dll_insertion.cc

---
+++ interview_prep/linked_lists/dll_insertion.cc
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+    DoublyLinkedListNode* cur = head;
+    DoublyLinkedListNode* previous = cur;
+    DoublyLinkedListNode* insert = new DoublyLinkedListNode(data);
+    if (data < head->data) {
+        insert->next = head;
+        head = insert;
+        return head;
+    }
+    while (cur) {
+        if (cur->data > data) {
+            insert->next = cur;
+            insert->prev = previous;
+            previous->next = insert;
+            cur->prev = insert;
+            return head;
+        }
+        cout << "moving to next position" << endl;
+        previous = cur;
+        cur = cur->next;
+    }
+    cout << "insert at the end of list" << endl;
+    previous->next = insert;
+    insert->prev = previous;
+    return head;
+
+}
+
+int main()
