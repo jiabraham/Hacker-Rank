f8ed39d40a40da24dfbc71e8a5fb9dd48c4a544c
Joseph I. Abraham <jia253@nyu.edu>
2019-09-21 20:39:49 -0400

    Update huffman_decoding.py

--- interview_prep/trees/huffman_decoding.py
+++ interview_prep/trees/huffman_decoding.py
@@ -1,49 +1,4 @@
-import queue as Queue
 
-cntr = 0
-
-class Node:
-    def __init__(self, freq, data):
-        self.freq = freq
-        self.data = data
-        self.left = None
-        self.right = None
-        global cntr
-        self._count = cntr
-        cntr = cntr + 1
-        
-    def __lt__(self, other):
-        if self.freq != other.freq:
-            return self.freq < other.freq
-        return self._count < other._count
-
-def huffman_hidden():#builds the tree and returns root
-    q = Queue.PriorityQueue()
-
-    
-    for key in freq:
-        q.put((freq[key], key, Node(freq[key], key) ))
-    
-    while q.qsize() != 1:
-        a = q.get()
-        b = q.get()
-        obj = Node(a[0] + b[0], '\0' )
-        obj.left = a[2]
-        obj.right = b[2]
-        q.put((obj.freq, obj.data, obj ))
-        
-    root = q.get()
-    root = root[2]#contains root object
-    return root
-
-def dfs_hidden(obj, already):
-    if(obj == None):
-        return
-    elif(obj.data != '\0'):
-        code_hidden[obj.data] = already
-        
-    dfs_hidden(obj.right, already + "1")
-    dfs_hidden(obj.left, already + "0")
 
 """class Node:
     def __init__(self, freq, data):
@@ -56,35 +11,30 @@ def dfs_hidden(obj, already):
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def decodeHuff(root, s):
 	#Enter Your Code Here
-    prev = root
     string = ""
-    doDecodeHuff(root, s, string, prev)
-    return string
+    prev = root
+    ret = doDecodeHuff(root, s, string, prev)
+    print(ret)
 
 #A for loop could potentially use less memory here, less args to pass in
+#1001011
 def doDecodeHuff(root, s, string, prev):
-    print("Init: " + s + " " + root.data)
-    if s[0:1] == "0":
-        print("Mat0: " + string)
-        if not root.left:
-            string += root.data
-            print("Mat!0: " + string)
-            doDecodeHuff(root, s, string, root)
-        else:
-            doDecodeHuff(root.left, s[1:len(s)], string, root)
-        print("0Match : " + string)
-        return
+    if s == "" and root.right and root.left:
+        return string
+    if root.right and root.left:
+        if s[0:1] == "1":
+            #print("going right")
+            return doDecodeHuff(root.right, s[1:], string, prev)
+        if s[0:1] == "0":
+            #print("going left")
+            return doDecodeHuff(root.left, s[1:], string, prev)
+    else:
+        string += root.data
+        #print("Updating string to: " + str(string))
+        #print("s = " + str(s[1:len(s)]))
+        return doDecodeHuff(prev, s, string, prev)
     
-    if s[0:1] == "1":
-        if not root.right:
-            string += root.data
-            doDecodeHuff(root, s, string, root)
-        else:
-            doDecodeHuff(root.right, s[1:len(s)], string, root)
-    print("End: " + string)
-    return
 
 
     
 
-ip = input()
